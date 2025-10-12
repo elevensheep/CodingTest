1275a05956c77d2b86ca2c8ad05b52b15e15380b
elevensheep <fttc100@gmail.com>
2025-10-12 21:09:45 +0900

    2798

---
+++ 2798.py
@@ -0,0 +1,14 @@
+# combinations 사용으로 조합 구하기기
+from itertools import combinations
+
+n, m = map(int, input().split())
+card = list(map(int, input().split()))
+
+result = 0
+
+for combo in combinations(card, 3):
+    card_sum = sum(combo)
+    if card_sum <= m:
+        result = max(result, card_sum)
+
+print(result)
