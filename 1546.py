0fbbd3bed5db57aec542de1309876cc88b545a4f
elevensheep <fttc100@gmail.com>
2025-10-22 19:01:06 +0900

    1546.py

---
+++ 1546.py
@@ -0,0 +1,10 @@
+# def func(arr):
+#     return max(arr)
+# n = int(input())
+# arr = list(map(int, input().split()))
+
+# if len(arr) == n:
+#     print(sum(arr)/len(arr))
+
+arr = [4, 2, 1, 5, 3]
+print(arr.pop(4))
